eabccf722cff0d023747c4cb365a1125908fd94d
VENKATESH PONNURU <130523792+438-venkatesh@users.noreply.github.com>
2024-05-21 15:16:53 +0530

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      we specialize in crafting exceptional experiences for all your special occasions, from birthday parties and marriage anniversaries to adventurous campaigning trips and thrilling gaming parties. With our dedicated team of event planners and coordinators, we're here to turn your dreams into reality and create memories that last a lifetime.
+      At Venky Events, our mission is simple: to bring joy, excitement, and unparalleled experiences to every event we organize. We believe that every occasion deserves to be celebrated in style, and we're committed to ensuring that your event is nothing short of extraordinary. Whether you're planning an intimate gathering or a grand celebration, we're here to make it happen with precision, creativity, and a touch of magic.
+
+      </p>
+    </section>
+  );
+};
+
+export default About;
